53180aa5218df79ae6e16e80511c8a4071cc868c
ChuckT43212 <nfamy24701@yahoo.com>
2016-07-13 20:27:02 +0000

    fixed redundency

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   
   
   
@@ -17,8 +19,7 @@ class ArticlesController < ApplicationController
   
   def edit
     
-    @article = Article.find(params[:id])
-    
+  
   end
   
   def create
@@ -37,7 +38,7 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
+    
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -48,17 +49,24 @@ class ArticlesController < ApplicationController
   
   
   def show
-    @article = Article.find(params[:id])
+    
   end
   
   def destroy
-    @article = Article.find(params[:id])
+    
     @article.destroy
     flash[:notice] = "Article was sucessfully deleted"
     redirect_to articles_path
   end
   
   private
+  
+  def set_article
+    
+  @article = Article.find(params[:id])  
+  end
+  
+  
   def article_params
     
     params.require(:article).permit(:title, :description)
