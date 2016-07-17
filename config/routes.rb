a8bfc876f46c767ea23ffe611e7b2030f4f3fb58
ChuckT43212 <nfamy24701@yahoo.com>
2016-07-17 16:58:34 +0000

    added update, edit functionality

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
-  #resources :users, except: [:new]
+  resources :users, except: [:new]
   
   
 
