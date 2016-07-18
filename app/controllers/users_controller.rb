5cb0e4ffbb0e04bf35c2cfc78867861954e985eb
ChuckT43212 <nfamy24701@yahoo.com>
2016-07-18 07:28:31 +0000

    complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action require_same_user, only: [:edit, :update]
   
   def index
     
@@ -35,11 +36,11 @@ class UsersController < ApplicationController
   
   
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
+   
     
     if @user.update(user_params)
       flash[:success] = 'Your account was updated successfully'
@@ -59,7 +60,7 @@ class UsersController < ApplicationController
   
   def show
     
-    @user = User.find(params[:id])
+   
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     
   end
@@ -69,6 +70,18 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
   
-
+  def set_user
+     @user = User.find(params[:id])
+    
+  end
+  
+def require_same_user
+  
+  if current_user != @user
+    flash[:danger] = 'You can only edit your own account'
+    redirect_to root_path
+  end
+  
+end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,17 @@ Rails.application.routes.draw do
   
   
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+  # post 'users', to: 'users#create'
   resources :users, except: [:new]
+  
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+
+  
+  
   
   
 
